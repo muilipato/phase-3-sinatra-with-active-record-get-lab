7d7c9ab0b8641857a6e19f0db69c9478879374c6
Patrick <muilipato@gmail.com>
2023-08-01 07:50:10 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+   bakeries = Bakery.all
+   bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+  get '/baked_goods/by_price' do
+    bakedgoods = BakedGood.order(price: :desc)
+    bakedgoods.to_json
+  end
+  get '/baked_goods' do
+    allbaked = BakedGood.all
+    allbaked.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expe = BakedGood.all.order(price: :desc).limit(1).first
+    most_expe.to_json
+  end
+
+
+
 
 end
